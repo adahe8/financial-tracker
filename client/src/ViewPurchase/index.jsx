48efef0c7ce416f6037249a013f3a20690148105
adahe8 <ada.he0908@gmail.com>
2022-12-30 19:40:18 -0800

    Linked frontend and backend

--- client/src/ViewPurchase/index.jsx
+++ client/src/ViewPurchase/index.jsx
@@ -1,93 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import API from '../API';
 
-const purchaseData = [
-  {
-    name: "Soup",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 1,
-    method: "Cash",
-  },
-  {
-    name: "Salad",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 1,
-    method: "Cash",
-  },
-  {
-    name: "Bread",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 1,
-    method: "Cash",
-  },
-  {
-    name: "Backpack",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 1,
-    method: "Cash",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-  {
-    name: "Shoe",
-    description: "Yum",
-    location: "CSE Basement",
-    date: "12/12/1234",
-    cost: 100000,
-    method: "Crypto",
-  },
-];
 
 const ViewPurchase = () => {
+
+  const [body, setBody] = useState([]);
+
+  useEffect(() => {
+    API.getPurchase().then((response) => {
+      console.log(response);
+      setBody(response.data.purchases);
+    });
+  }, []);
+
   return (
     <div className="view-container">
-      {purchaseData.map((purchase, index) => {
+      {body.map((purchase) => {
         return (
           <div className="view-item">
             <h1 className="item-title">{purchase.name}</h1>
